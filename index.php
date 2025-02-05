263b90662f0ed027b3fe4293706b068ee261b198
GodorojaOxana2004 <145650682+GodorojaOxana2004@users.noreply.github.com>
2025-02-05 13:19:26 +0200

    Update index.php

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 $days = 288;
 $message = "Все возвращаются на работу!";
 
-echo  $days . $message "<br>";
+echo  "через" $days "дней" . $message "<br>";
 echo  "$days . $message<br>";
 
-print $days . $message "<br>";
-print  "$days . $message "<br>";
+print "через" $days "дней" . $message "<br>";
+print  "$days . $message <br>";
 ?>
