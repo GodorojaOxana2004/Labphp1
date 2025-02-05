2cd9ff19373300604a7ef41af279095fa347396a
GodorojaOxana2004 <145650682+GodorojaOxana2004@users.noreply.github.com>
2025-02-05 10:21:57 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,11 @@
+<?php
+
+$days = 288;
+$message = "Все возвращаются на работу!";
+
+echo  $days . $message "<br>";
+echo  "$days . $message<br>";
+
+print $days . $message "<br>";
+print  "$days . $message "<br>";
+?>
